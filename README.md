d51cfa2477f521fbb719f5b645495fdee7c6c5ab
Denise Case <denisecase@gmail.com>
2024-02-23 08:40:12 -0600

    reactive

--- README.md
+++ README.md
@@ -5,9 +5,31 @@
 
 Run and publish interactive apps using PyShiny Express and GitHub Pages.
 
+## Data Description
+
+This app uses the Seaborn library to import the tips dataset. The dataset contains 244 records and 7 fields. The fields are:
+
+Column names for the tips dataset include:
+
+- total_bill: the bill amount (dollars)
+- tip: the tip amount (dollars)
+- sex: Male or Female
+- smoker: Yes or No
+- day: Day of the week
+- time: Lunch or Dinner
+- size: Size of the party
+
+See: <https://github.com/mwaskom/seaborn-data/blob/master/tips.csv>
+
+## Data Cleaning and Transformation
+
+The tips dataset is a nice, clean dataset with no missing values.
+Generally, no cleaning or transformation is needed.
+
 ## Source
 
-From <https://shiny.posit.co/py/docs/user-interfaces.html#altogether-now>
+From <https://shiny.posit.co/py/docs/user-interfaces.html#altogether-now>.
+This version has been modified slightly for hosting with GitHub Pages.
 
 ## Prerequisites
 
@@ -43,13 +65,23 @@ git config user.email
 
 ## Python Project Virtual Environment
 
-1. Create a virtual environment for the project:
+Run the following commands in the terminal to set up a project virtual environment and install the necessary packages.
+Run them from the root project folder (e.g. pyshiny-tips-dashboard-express).
+Use PowerShell on Windows or the terminal on macOS and Linux.
+
+### Create a Project Virtual Environment (generally one-time setup)
+
+Create a project virtual environment in the .venv folder of the project root directory.
 
   ```shell
   py -m venv .venv`
   ```
 
-2. Activate the virtual environment
+### Activate the Project Virtual Environment (when you work on the project)
+
+Once the project virtual environment exists,
+ we activate the virtual environment to work on the project - or when we open a new terminal.
+We can verify our environment is active when (.venv) appears in the terminal prompt.
 
 On Windows:
 
@@ -63,7 +95,18 @@ On macOS and Linux:
   source .venv/bin/activate
   ```
 
-Install packages:
+We also need to select this project virtual environment in VS Code.
+To do this, open the command palette (Ctrl+Shift+P) and search for "Python: Select Interpreter". Then, select the .venv folder in the project root directory.
+
+### Install Packages into the Active Project Virtual Environment
+
+When the project virtual environment is active,
+install packages into the project virtual environment so they are available for use in the Python code.
+
+NOTE:
+
+- We **install** packages into the project virtual environment.
+- We **import** packages into Python code (after they have been installed).
 
 First, upgrade pip and wheel for good measure.
 Then, install the project-specific required packages:
@@ -75,7 +118,9 @@ py -m pip install --upgrade -r requirements.txt
 
 ## Run the App
 
-With your project virtual environment activated in the terminal and the necessary packages installed, run the app with live reloading and automatically open it in the browser:
+With your project virtual environment activated in the terminal
+ and the necessary packages installed, run the app with live reloading and
+ automatically open it in the browser:
 
 ```shell  
 shiny run --reload --launch-browser tips/app.py
@@ -84,18 +129,33 @@ shiny run --reload --launch-browser tips/app.py
 While the app is running, that terminal is fully occupied.
 Open a new terminal to run other commands.
 
-## Build the App
+## Build the App to Docs Folder and Test Locally
 
-With your project virtual environment activated in the terminal and the necessary packages installed, remove any existing assets and use shinylive export to build the app in tips folder to the docs folder:
+With your project virtual environment activated in the terminal
+ and the necessary packages installed, remove any existing assets and use
+ shinylive export to build the app in the tips folder to the docs folder:
 
 ```shell
 shiny static-assets remove
 shinylive export tips docs
 ```
 
-## Publish the App with GitHub Pages
+After the app is built, serve the app locally from the docs folder to test before publishing to GitHub Pages.
+In the terminal, run the following command from the root of the project folder:
 
-1. Git add/commit/push changes to the main branch:
+```shell
+py -m http.server --directory docs --bind localhost 8008
+```
+
+Open a browser (tested with Chrome) and navigate to [http://localhost:8008](http://localhost:8008) to view the app running locally.
+
+## After Editing, Git Add/Commit/Push Changes to GitHub
+
+After editing project files, use Git add/commit/push changes to the main branch of the repository.
+Note that if a terminal is serving an app, it is not available for other commands.
+Run the following commands from a new or available terminal to git add/commit/push changes to GitHub.
+Replace "Your commit message" with a meaningful message about the changes you made to the project files.
+Run commands one at a time and wait for each to complete before running the next.
 
 ```shell
 git add .
@@ -103,7 +163,19 @@ git commit -m "Your commit message"
 git push -u origin main
 ```
 
-2. Go to the repository on GitHub and navigate to the Settings tab.
-3. Scroll down to the GitHub Pages section and select the main branch as the source for the site.
-4. Publish from the docs folder.
+## Publish the App with GitHub Pages (one-time setup)
+
+The first time you set up an app, you'll need to navigate to the repository on GitHub and configure the settings to publish the app with GitHub Pages.
+After configuring the repository once, each time you push changes to the main branch, the app will automatically update.
+
+1. Go to the repository on GitHub and navigate to the **Settings** tab.
+2. Scroll down and click the **Pages** section.
+3. Select branch **main** as the source for the site.
+4. Select the **docs** folder to publish from.
 5. Click Save and wait for the site to build.
+6. Edit the "About" section of the repository to include a link to the live app.
+
+## Resources
+
+Example csv data from [tips.csv](https://github.com/mwaskom/seaborn-data/blob/master/tips.csv).
+Used for review only. In the app, we import the data from the Seaborn library.
--- tips/app.py
+++ tips/app.py
@@ -1,68 +1,162 @@
-import faicons as fa
-import pandas as pd # required for plotly.express
-import plotly.express as px
-from shinywidgets import render_plotly
-
-from shiny import reactive, render, req
-from shiny.express import input, ui
-
+# COMPATIBILITY ISSUES:
 # Plotly's px.scatter function uses statsmodels for lowess trendlines under the hood.
-# import statsmodels.api as sm # required for total bill vs tip plotly.express charts
+# Which won't work when hosting on GitHub Pages, so we'll comment it out.
 
+# IMPORT the packages we need (or might want to use) first.
+# NOTE: Each package should be added to requirements.txt,
+#       so the packages can be INSTALLED into the project virtual environment.
 
-# Load data and compute static values
-tips = px.data.tips()
-bill_rng = (min(tips.total_bill), max(tips.total_bill))
+import faicons as fa  # For using font awesome in cards
+import pandas as pd  # Pandas for data manipulation, required by plotly.express
+import plotly.express as px  # Plotly Express for making Plotly plots
+from shinywidgets import render_plotly  # For rendering Plotly plots
+from shiny import reactive, render, req  # To define reactive calculations
+from shiny.express import input, ui  # To define the user interface
 
-# Add page title and sidebar
-ui.page_opts(title="Restaurant tipping", fillable=True)
-with ui.sidebar(open="desktop"):
-    ui.input_slider("total_bill", "Bill amount", min=bill_rng[0], max=bill_rng[1], value=bill_rng, pre="$")
-    ui.input_checkbox_group("time", "Food service", ["Lunch", "Dinner"], selected=["Lunch", "Dinner"], inline=True)
-    ui.input_action_button("reset", "Reset filter")
+# ALWAYS familiarize yourself with the data you are working with first.
+# Column names for the tips dataset include:
+# - total_bill: the bill amount (dollars)
+# - tip: the tip amount (dollars)
+# - sex: Male or Female
+# - smoker: Yes or No
+# - day: Day of the week
+# - time: Lunch or Dinner
+# - size: Size of the party
+
+# Load data into a pandas DataFrame
+# Use the tips dataset from Plotly Express
+tips_df = px.data.tips()
+
+# Compute static values
+bill_range_tuple = (min(tips_df.total_bill), max(tips_df.total_bill))
+
+# Define the Shiny UI Page layout
+# Call the ui.page_opts() function to set the page title and make the page fillable
+ui.page_opts(title="PyShiny Express: Restaurant Tipping Example", fillable=True)
+
+# Add a Shiny UI sidebar for user interaction
+# Use the ui.sidebar() function to create a sidebar
+# Set the open parameter to "open" to make the sidebar open by default
+# Use a with block to add content to the sidebar
+# Using Shiny Express there are no punctuation between ui elements
+# Use the ui.h2() function to add a 2nd level header to the sidebar
+#   pass in a string argument (in quotes) to set the header text
+# Use the ui.input_slider() function to add a slider to the sidebar
+#   pass in a string argument (in quotes) to set the input label
+#   pass in a string argument (in quotes) to set the input label
+#   pass in a tuple argument to set the minimum and maximum values
+#   pass in a string argument (in quotes) to set the prefix
+# Use the ui.input_checkbox_group() function to add a checkbox group to the sidebar
+#   pass in a string argument (in quotes) to set the input label
+#   pass in a list argument to set the input options
+#   pass in a list argument to set the selected options
+#   pass in a boolean argument to set the inline option
+# Use the ui.input_action_button() function to add an action button to the sidebar
+#   pass in a string argument (in quotes) to set the button label
+# Use the ui.hr() function to add a horizontal rule to the sidebar
+# Use the ui.h6() function to add a 6th level header to the sidebar
+#   pass in a string argument (in quotes) to set the header text
+# Use ui.a() to add a hyperlink to the sidebar
+#   pass in two arguments:
+#   the text for the hyperlink (in quotes)
+#   the URL for the hyperlink (in quotes)
+#   set the target parameter to "_blank" to open the link in a new tab
+# When passing in multiple arguments to a function, separate them with commas.
+
+with ui.sidebar(open="open"):
+
+    ui.h2("Sidebar")
+
+    ui.input_slider(
+        "selected_range_total_bill",
+        "Bill amount",
+        min=bill_range_tuple[0],
+        max=bill_range_tuple[1],
+        value=bill_range_tuple,
+        pre="$",
+    )
+
+    ui.input_checkbox_group(
+        "selected_time_category",
+        "Food service",
+        ["Lunch", "Dinner"],
+        selected=["Lunch", "Dinner"],
+        inline=True,
+    )
+
+    ui.input_action_button("reset_event", "Reset filter")
+    ui.hr()
+    ui.h6("Links:")
+    ui.a(
+        "GitHub Source",
+        href="https://github.com/denisecase/pyshiny-tips-dashboard-express",
+        target="_blank",
+    )
+    ui.a(
+        "GitHub App",
+        href="https://denisecase.github.io/pyshiny-tips-dashboard-express/",
+        target="_blank",
+    )
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a(
+        "PyShiny Express",
+        href="hhttps://shiny.posit.co/blog/posts/shiny-express/",
+        target="_blank",
+    )
+    ui.a(
+        "See the Code",
+        href="https://shiny.posit.co/py/docs/user-interfaces.html#all-together-now",
+        target="_blank",
+    )
 
 # Add main content
 ICONS = {
     "user": fa.icon_svg("user", "regular"),
     "wallet": fa.icon_svg("wallet"),
     "currency-dollar": fa.icon_svg("dollar-sign"),
-    "gear": fa.icon_svg("gear")
+    "gear": fa.icon_svg("gear"),
 }
 
+# Value boxes
 with ui.layout_columns(fill=False):
 
     with ui.value_box(showcase=ICONS["user"]):
         "Total tippers"
+
         @render.express
         def total_tippers():
-            tips_data().shape[0]
+            filtered_data().shape[0]
 
     with ui.value_box(showcase=ICONS["wallet"]):
         "Average tip"
+
         @render.express
         def average_tip():
-            d = tips_data()
+            d = filtered_data()
             if d.shape[0] > 0:
                 perc = d.tip / d.total_bill
                 f"{perc.mean():.1%}"
 
     with ui.value_box(showcase=ICONS["currency-dollar"]):
         "Average bill"
+
         @render.express
         def average_bill():
-            d = tips_data()
+            d = filtered_data()
             if d.shape[0] > 0:
                 bill = d.total_bill.mean()
                 f"${bill:.2f}"
 
 
+# Tables and charts
 with ui.layout_columns(col_widths=[6, 6, 12]):
 
     with ui.card(full_screen=True):
         ui.card_header("Tips data")
+
         @render.data_frame
         def table():
-            return render.DataGrid(tips_data())
+            return render.DataGrid(filtered_data())
 
     with ui.card(full_screen=True):
         with ui.card_header(class_="d-flex justify-content-between align-items-center"):
@@ -70,16 +164,17 @@ with ui.layout_columns(col_widths=[6, 6, 12]):
             with ui.popover(title="Add a color variable", placement="top"):
                 ICONS["gear"]
                 ui.input_radio_buttons(
-                    "scatter_color", None,
+                    "scatter_color",
+                    None,
                     ["none", "sex", "smoker", "day", "time"],
-                    inline=True
+                    inline=True,
                 )
 
         @render_plotly
         def scatterplot():
             color = input.scatter_color()
             return px.scatter(
-                tips_data(),
+                filtered_data(),
                 x="total_bill",
                 y="tip",
                 color=None if color == "none" else color,
@@ -92,70 +187,121 @@ with ui.layout_columns(col_widths=[6, 6, 12]):
             with ui.popover(title="Add a color variable"):
                 ICONS["gear"]
                 ui.input_radio_buttons(
-                    "tip_perc_y", "Split by:",
+                    "tip_perc_y",
+                    "Split by:",
                     ["sex", "smoker", "day", "time"],
                     selected="day",
-                    inline=True
+                    inline=True,
                 )
 
         @render_plotly
         def tip_perc():
-            dat = tips_data()
-            dat["percent"] = dat.tip / dat.total_bill
+            filtered_df = filtered_data()
+            filtered_df["percent"] = filtered_df.tip / filtered_df.total_bill
             yvar = input.tip_perc_y()
-         
-            # uvals = dat[yvar].unique()
 
-            # samples = [
-            #     [ dat.percent[dat[yvar] == val] ]
-            #     for val in uvals
-            # ]
-
-            # plt = ridgeplot(
-            #     from ridgeplot import ridgeplot
-            #     samples=samples, labels=uvals, bandwidth=0.01,
-            #     colorscale="viridis", colormode="row-index"
-            # )
-
-            # plt.update_layout(
-            #     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5)
-            # )
-
-            # return plt
-        
             # Create a violin plot with Plotly
-            fig = px.violin(
-                dat,
+            violin_figure = px.violin(
+                filtered_df,
                 y="percent",
                 color=yvar,  # This will split the violin plot by the selected variable
                 box=True,  # Displays a box plot inside the violin
                 points="all",  # Shows all points
-                hover_data=tips.columns,  # Adds all other data as hover information
-                title="Distribution of Tip Percentages by " + yvar.capitalize()
+                hover_data=tips_df.columns,  # Adds all other data as hover information
+                title="Distribution of Tip Percentages by " + yvar.capitalize(),
             )
-    
+
             # Update layout for better readability
-            fig.update_layout(
+            violin_figure.update_layout(
                 yaxis_title="Tip Percentage",
                 legend_title=yvar.capitalize(),
-                legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+                legend=dict(
+                    orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1
+                ),
             )
-            return fig
+            return violin_figure
 
 
 # --------------------------------------------------------
 # Reactive calculations and effects
 # --------------------------------------------------------
 
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on filtered_data() will be updated when the data changes.
+
+# In this case, notice that many outputs depend on filtered_data().
+
+
 @reactive.calc
-def tips_data():
-    bill = input.total_bill()
-    idx1 = tips.total_bill.between(bill[0], bill[1])
-    idx2 = tips.time.isin(input.time())
-    return tips[idx1 & idx2]
+def filtered_data():
+
+    # Get the selected range from the input as a tuple (min, max)
+    selected_tuple = input.selected_range_total_bill()
+
+    # Use the between() method to filter the DataFrame
+    # The method returns a boolean Series with the same index as the original DataFrame
+    # Each row is:
+    #   True if the total bill is in the input.selected_range_total_bill()
+    #   False if the total bill is not
+    isTotalBillInRange = tips_df.total_bill.between(
+        selected_tuple[0], selected_tuple[1]
+    )
+
+    # Use the isin() method to filter the DataFrame
+    # The method returns a boolean Series with the same index as the original DataFrame
+    # Each row is:
+    #   True if the row is in the input.selected_species() list
+    #   False if the species is not
+    isInSelectedTime = tips_df.time.isin(input.selected_time_category())
+
+    # Use the boolean filter mask in square brackets to filter the DataFrame
+    # Return the filtered DataFrame when the function is triggered
+    # Filter masks can be combined with the & operator for AND and the | operator for OR
+    return tips_df[isTotalBillInRange & isInSelectedTime]
+
 
 @reactive.effect
-@reactive.event(input.reset)
+@reactive.event(input.reset_event)
 def _():
-    ui.update_slider("total_bill", value=bill_rng)
-    ui.update_checkbox_group("time", selected=["Lunch", "Dinner"])
+    ui.update_slider("selected_range_total_bill", value=bill_range_tuple)
+
+    ui.update_checkbox_group("selected_time_category", selected=["Lunch", "Dinner"])
+
+
+# Additional Python Notes
+# ------------------------
+# Capitalization matters in Python. Python is case-sensitive: min and Min are different.
+# Spelling matters in Python. You must match the spelling of functions and variables exactly.
+# Indentation matters in Python. Indentation is used to define code blocks and must be consistent.
+
+# Functions
+# ---------
+# Functions are used to group code together and make it more readable and reusable.
+# We define custom functions that can be called later in the code.
+# Functions are blocks of logic that can take inputs, perform work, and return outputs.
+
+# Defining Functions
+# ------------------
+# Define a function using the def keyword, followed by the function name, parentheses, and a colon. 
+# The function name should describe what the function does.
+# In the parentheses, specify the inputs needed as arguments the function takes.
+# For example:
+#    The function filtered_data() takes no arguments.
+#    The function between(min, max) takes two arguments, a minimum and maximum value.
+#    Arguments can be positional or keyword arguments, labeled with a parameter name.
+
+# The function body is indented (consistently!) after the colon. 
+# Use the return keyword to return a value from a function.
+    
+# Calling Functions
+# -----------------
+# Call a function by using its name followed by parentheses and any required arguments.
+    
+# Decorators
+# ----------
+# Use the @ symbol to decorate a function with a decorator.
+# Decorators a concise way of calling a function on a function.
+# We don't typically write decorators, but we often use them.
+    
